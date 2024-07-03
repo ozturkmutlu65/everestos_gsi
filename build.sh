988238ad18375cddc5a17c57ee1ccb82428fbb4c
Imbroglius <163344413+Imbroglius@users.noreply.github.com>
2024-07-03 12:26:39 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -131,7 +131,7 @@ generateOta() {
             [[ "$filename" == *"-vndklite"* ]] && vndk="-vndklite" || vndk=""
             name="treble_${arch}_b${variant}N${vndk}"
             size=$(wc -c $file | awk '{print $1}')
-            url="https://github.com/ponces/treble_aosp/releases/download/$version/$filename"
+            url="https://github.com/Imbrogliu/Redmi_13C_treble_aosp/releases/download/$version/$filename"
             json="${json} {\"name\": \"$name\",\"size\": \"$size\",\"url\": \"$url\"},"
         done
         json="${json%?}]}"

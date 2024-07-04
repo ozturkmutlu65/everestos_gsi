48fd5622a549657aec35f3df6ef7fa39c177149d
Imbroglius <163344413+Imbroglius@users.noreply.github.com>
2024-07-04 10:31:10 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -39,7 +39,7 @@ uploadAssets() {
         curl -o /dev/null -s -L -X POST \
             "https://uploads.github.com/repos/$GUSER/$GREPO/releases/$id/assets?name=$(basename $file)" \
             -H "Accept: application/vnd.github+json" \
-            -H "Authorization: token $GITHUB_API_TOKEN" \
+            -H "Authorization: Bearer $GITHUB_API_TOKEN" \
             -H "Content-Type: application/octet-stream" \
             -T "$file"
         echo

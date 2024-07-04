6e3a45e6ecab797c5d62d5c94a24f70accff71df
Imbroglius <163344413+Imbroglius@users.noreply.github.com>
2024-07-04 15:57:00 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -15,7 +15,7 @@ BD=$HOME/builds
 TAG="$(date +v%Y.%m.%d)"
 GUSER="Imbroglius"
 GREPO="Redmi_13C_treble_aosp"
-AUTH="Authorization: token $github_token"
+AUTH="Authorization: token ${{ secrets.GITHUB_TOKEN }}"
 
 
 SKIPOTA=false

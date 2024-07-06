b2a4e247e6fe22ffc87ffe8cdc7b7c59fa78dfaf
imbroglius <163344413+imbroglius@users.noreply.github.com>
2024-07-06 16:25:47 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -15,6 +15,7 @@ BD=$HOME/builds
 TAG="$(date +v%Y.%m.%d)"
 GUSER="imbroglius"
 GREPO="redmi_13c_treble_aosp"
+GH_TOKEN: ${{ secrets.GITHUB_TOKEN }}
 
 
 
@@ -28,7 +29,7 @@ createRelease() {
     res=$(curl -s -L -X POST \
         "https://api.github.com/repos/$GUSER/$GREPO/releases" \
         -H "Accept: application/vnd.github+json" \
-        -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" \
+        -H "Authorization: token $GH_TOKEN" \
         -d "{\"tag_name\":\"$TAG\",\"name\":\"AOSP 14.0 $TAG\",\"body\":\"## Changelog\n- ...\n\n## Notes\n- ...\",\"draft\":true}")
     id=$(echo "$res" | jq -rc ".id")
     echo
@@ -41,7 +42,7 @@ uploadAssets() {
         curl -o /dev/null -s -L -X POST \
             "https://uploads.github.com/repos/$GUSER/$GREPO/releases/$id/assets?name=$(basename $file)" \
             -H "Accept: application/vnd.github+json" \
-            -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" \
+            -H "Authorization: token $GH_TOKEN" \
             -H "Content-Type: application/octet-stream" \
             -T "$file"
         echo

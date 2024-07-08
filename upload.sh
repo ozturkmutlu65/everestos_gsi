9d6bd05a4bad6ac17a3374892a9ca4fc3f93676b
imbroglius <163344413+imbroglius@users.noreply.github.com>
2024-07-08 23:39:44 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -15,6 +15,7 @@ BD=$HOME/builds
 TAG="$(date +v%Y.%m.%d)"
 GUSER="imbroglius"
 GREPO="imbroglios_gsi"
+TOKKEN=${{ secrets.IMBROGLIOTOKEN }}
 
 
 SKIPOTA=false
@@ -27,7 +28,7 @@ createRelease() {
     res=$(curl -s -L -X POST \
         "https://api.github.com/repos/$GUSER/$GREPO/releases" \
         -H "Accept: application/vnd.github+json" \
-        -H "Authorization: token ${{ secrets.IMBROGLIOTOKEN }}" \
+        -H "Authorization: Bearer $TOKKEN" \
         -d "{\"tag_name\":\"$TAG\",\"name\":\"AOSP 14.0 $TAG\",\"body\":\"## Changelog\n- ...\n\n## Notes\n- ...\",\"draft\":true}")
     id=$(echo "$res" | jq -rc ".id")
     echo
@@ -40,7 +41,7 @@ uploadAssets() {
         curl -o /dev/null -s -L -X POST \
             "https://uploads.github.com/repos/$GUSER/$GREPO/releases/$id/assets?name=$(basename $file)" \
             -H "Accept: application/vnd.github+json" \
-            -H "Authorization: token ${{ secrets.IMBROGLIOTOKEN }}" \
+            -H "Authorization: Bearer $TOKKEN" \
             -H "Content-Type: application/octet-stream" \
             -T "$file"
         echo

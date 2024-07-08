a8a8c43a654ab8e6b2b1a4f97dbbf477d1ae23d7
imbroglius <163344413+imbroglius@users.noreply.github.com>
2024-07-08 15:27:19 +0200

    corrected Imbroglio to imbroglius

--- build.sh
+++ build.sh
@@ -131,7 +131,7 @@ generateOta() {
             [[ "$filename" == *"-vndklite"* ]] && vndk="-vndklite" || vndk=""
             name="treble_${arch}_b${variant}N${vndk}"
             size=$(wc -c $file | awk '{print $1}')
-            url="https://github.com/Imbrogliu/imbroglios_gsi/releases/download/$version/$filename"
+            url="https://github.com/imbroglius/imbroglios_gsi/releases/download/$version/$filename"
             json="${json} {\"name\": \"$name\",\"size\": \"$size\",\"url\": \"$url\"},"
         done
         json="${json%?}]}"

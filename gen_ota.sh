6a4f8c92d118e9ad5e93601651ea6e156bc2c2c7
imbroglius <163344413+imbroglius@users.noreply.github.com>
2024-07-12 13:41:16 +0200

    Create gen_ota.sh

---
+++ gen_ota.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo
+echo "--------------------------------------"
+echo "          AOSP 14.0 Buildbot          "
+echo "              ImbrogliOS              "
+echo "             by Imbroglius            "
+echo "--------------------------------------"
+echo
+
+set -e
+
+BL=$PWD/imbroglios_gsi
+BD=$HOME/builds
+BV=$1
+
+
+generateOta() {
+    echo "--> Generating OTA file"
+    version="$(date +v%Y.%m.%d)"
+    buildDate="$(date +%Y%m%d)"
+    timestamp="$START"
+    json="{\"version\": \"$version\",\"date\": \"$timestamp\",\"variants\": ["
+    find $BD/ -name "ImbrogliOS_aosp-*-14.0-$buildDate.img.xz" | sort | {
+        while read file; do
+            filename="$(basename $file)"
+            [[ "$filename" == *"-arm32"* ]] && arch="a64" || arch="arm64"
+            [[ "$filename" == *"-vanilla"* ]] && variant="v" || variant="g"
+            [[ "$filename" == *"-vndklite"* ]] && vndk="-vndklite" || vndk=""
+            name="treble_${arch}_b${variant}N${vndk}"
+            size=$(wc -c $file | awk '{print $1}')
+            url="https://github.com/imbroglius/imbroglios_gsi/releases/download/$version/$filename"
+            json="${json} {\"name\": \"$name\",\"size\": \"$size\",\"url\": \"$url\"},"
+        done
+        json="${json%?}]}"
+        echo "$json" | jq . > $BL/config/ota.json
+    }
+    echo

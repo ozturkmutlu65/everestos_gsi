859ee228769985efbf6431f5e125495e3f1f9625
imbroglius <163344413+imbroglius@users.noreply.github.com>
2024-07-14 10:55:50 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ initRepos() {
 
 syncRepos() {
     echo "--> Syncing repos"
-    repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all) --fail-fast || repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+    repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all) || repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
     echo
 }
 
